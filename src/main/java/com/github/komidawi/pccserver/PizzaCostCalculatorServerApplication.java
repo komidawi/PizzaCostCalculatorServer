f233dc2e0de4d61782349d999a11196c50aff325
Dawid Komisarczuk <komidawi@gmail.com>
2020-09-17 12:26:09 +0200

    Initial commit

---
+++ src/main/java/com/github/komidawi/pccserver/PizzaCostCalculatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.komidawi.pccserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaCostCalculatorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaCostCalculatorServerApplication.class, args);
+	}
+
+}

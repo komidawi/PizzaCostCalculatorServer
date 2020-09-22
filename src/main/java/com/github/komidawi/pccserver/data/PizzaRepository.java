28b997140cb2f983369ff1b9f8db16e1bab92412
Dawid Komisarczuk <komidawi@gmail.com>
2020-09-23 00:20:04 +0200

    [PCS-1] Feat: Add REST API 404 Not Found handling

--- src/main/java/com/github/komidawi/pccserver/data/PizzaRepository.java
+++ src/main/java/com/github/komidawi/pccserver/data/PizzaRepository.java
@@ -3,4 +3,7 @@ package com.github.komidawi.pccserver.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
+
+    Long deletePizzaById(Long id);
+
 }
--- src/main/java/com/github/komidawi/pccserver/service/PizzaService.java
+++ src/main/java/com/github/komidawi/pccserver/service/PizzaService.java
@@ -2,11 +2,11 @@ package com.github.komidawi.pccserver.service;
 
 import com.github.komidawi.pccserver.data.Pizza;
 import com.github.komidawi.pccserver.data.PizzaRepository;
-import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
 public class PizzaService {
@@ -17,22 +17,27 @@ public class PizzaService {
         this.repository = repository;
     }
 
+    @Transactional
     public List<Pizza> getAll() {
         return repository.findAll();
     }
 
-    public Pizza getById(Long id) {
-        return repository.findById(id).get();
+    @Transactional
+    public Optional<Pizza> getById(Long id) {
+        return repository.findById(id);
     }
 
+    @Transactional
     public Pizza save(Pizza newPizza) {
         return repository.save(newPizza);
     }
 
-    public void delete(Long id) {
-        repository.deleteById(id);
+    @Transactional
+    public Long deleteById(Long id) {
+        return repository.deletePizzaById(id);
     }
 
+    @Transactional
     public void clear() {
         repository.deleteAll();
     }
